3c3465fd0973d834a1c148680769974699c852da
AdilWhy <114363664+AdilWhy@users.noreply.github.com>
2023-03-01 22:20:14 +0600

    Add files via upload

---
+++ Labs/Lab 5/1.py
@@ -0,0 +1,25 @@
+import re
+
+txt = input()
+
+# t = re.findall('ab*', txt)
+
+# t = re.findall('ab{2,3}', txt)
+
+# t = re.findall('[a-z]+_[a-z]+', txt)
+
+# t = re.findall('[A-Z][a-z]+', txt)
+
+# t = re.findall('a.*b$', txt)
+
+# t = re.sub('[ ,.]', ':', txt)
+
+# t = re.sub(re.compile('(?:^|_)(.)'), lambda x: x.group(1).upper(), txt)
+
+# t = re.findall('[A-Z][^A-Z]*', txt)
+
+# t = re.sub('(?<!^)(?=[A-Z])', ' ', txt)
+
+# t = re.sub(re.compile('(?<!^)(?=[A-Z])'), '_', txt).lower()
+
+print(t)
